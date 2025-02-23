d0be3cc8b0b07eb83509ac8455a1ffdb7580328b
MaxO <maxi.opel.16@gmail.com>
2025-02-23 15:19:48 +0100

    Upating the get_equation function to take an actual input and return it.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def calculate_new_column(colum, number):
     for element in colum:
         new_colum.append(element * number)
 
-    print(f"New colum  :{colum,} to {new_colum} with {number}")
+    #print(f"New colum  :{colum,} to {new_colum} with {number}")
     return new_colum
 
 
@@ -39,13 +39,13 @@ def subtract_columns(colum0, colum1):
     for i in range(len(colum0)):
         new_colum.append(colum0[i] - colum1[i])
 
-    print(f"Subtraction: {colum0} - {colum1} = {new_colum}")
+    #print(f"Subtraction: {colum0} - {colum1} = {new_colum}")
     return new_colum
 
 
 
 def matrix_zeros(matrix):
-    changed_matrix = []
+
 
     #find the max colum number
     max_colum_number = 0
@@ -57,34 +57,34 @@ def matrix_zeros(matrix):
     for row in matrix[0]:
         max_row_number += 1
 
-    for row in range(max_row_number-2): #eins weniger wegen der informatik schreibweise und eines weils weniger sein soll
-        print(f"changing ROW to {row}")
+    for row in range(max_row_number-2):  #One less cause ones of the IT counting from 0 and one because the solution elements don´t need to be changed
+        #print(f"changing ROW to {row}")
 
-        for colum in range(1, max_colum_number): #first colum always stays the same
-            print(f"changing COLUM to {colum}")
+        for colum in range(1, max_colum_number): #first colum always stays the same so we start at index 1
+            #print(f"changing COLUM to {colum}")
 
             if row == 0: #then we have the first row always calculated with the zero colum
-                first_colum = calculate_new_column(matrix[0], matrix[colum][row])
+                #first_colum = calculate_new_column(matrix[0], matrix[colum][row])
                 #print(f"First:{first_colum} number was {matrix[colum][row]}")
-                new_colum = calculate_new_column(matrix[colum], matrix[0][row])
+                #new_colum = calculate_new_column(matrix[colum], matrix[0][row])
                 # print(f"New:{new_colum}")
-                co = subtract_columns(first_colum, new_colum)
+                matrix[colum]= subtract_columns(calculate_new_column(matrix[0], matrix[colum][row]),  calculate_new_column(matrix[colum], matrix[0][row]) )
                 # print(co)
-                matrix[colum] = co
+                #matrix[colum] = co
 
-            elif colum == max_colum_number-1: #if it is the last column we break
+            elif colum == max_colum_number-1: #if it is the last column we break because we already changed it. See further comments
                 #print(max_colum_number, colum)
                 break
 
-            else:  # for the other columns we need the colum from one above but for the right zero colum we can go one up
-                colum = row+1 #the colum we want to change with the zeros is one more below
-                print(f"actuall colum {colum}")
-                column_above = calculate_new_column(matrix[colum-1], matrix[colum][row])
-                colum_here = calculate_new_column(matrix[colum], matrix[colum-1][row])
-                co = subtract_columns(column_above,colum_here)
-                matrix[colum] = co
+            else:
+                colum = row+1 #the colum where the number needs to be changed to 0, is the row index+1.
+                #print(f"actual colum {colum}")
+                #column_above = calculate_new_column(matrix[colum-1], matrix[colum][row])
+                #colum_here = calculate_new_column(matrix[colum], matrix[colum-1][row])
+                matrix[colum]= subtract_columns(calculate_new_column(matrix[colum-1], matrix[colum][row]), calculate_new_column(matrix[colum], matrix[colum-1][row]))
+                #matrix[colum] = co
 
-            print(matrix)
+            #print(matrix)
     return matrix
 
 
@@ -92,15 +92,34 @@ def matrix_zeros(matrix):
 
 
 def get_equations(): #ask the user for equations
-    pass
+    print(f"Hello there! \n"
+          f"Plz insert yor equations one by one! \n"
+          f"When you are finished press ENTER. \n"
+          f"The Equations NEED to be typed in the form \n"
+          f"3x+2y + z = 1 \n"
+          f"4x-1y+3z = 11 \n"
+          f"so that every variable has a number in front of it, \n"
+          f"the solutions are on the right side of the = \n"
+          f"and the variables are in order in each equation!")
+
+    equations_list = []
+    eq = None
+
+    while eq != "":
+        print("Whats your equation: ")
+        eq = str(input())
+        if eq == "":
+            pass
+        else:
+            equations_list.append(eq)
+
+    return equations_list
 
 
 
-
+#eqs = get_equations()
 eqs = ["2x + 3y + 1z = 1","4x-1y+3z =11", "3x+1y-1z = 0"]
 Matrix =equations_to_matrix(eqs)
 print(Matrix)
 changed = matrix_zeros(Matrix)
 print(changed)
-
-
